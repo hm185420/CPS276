cb65304400fe8ecaded9fbfb2851a45dddda2b5b
HyoJin Moon <hmoon3@wccnet.edu>
2024-10-07 19:45:27 -0400

    updates

---
+++ assignments/assignment3/Calculator.php
@@ -0,0 +1,47 @@
+<?php
+    class Calculator
+    {
+            public function calc($opp = null, $int1 = null, $int2 = null)
+            {
+                try
+                {
+                    if($opp === null || $int1 === null || $int2 === null)
+                    {
+                        throw new Exception("You must enter a string and two numbers <br/>");
+                    }
+                    else if($opp == "+")
+                    {
+                        echo "The sum of the numbers is " . ($int1 + $int2) . "<br/>";
+                    } 
+                    else if($opp == "-")
+                    {
+                        echo "The difference of the numbers is " . ($int1 - $int2) . "<br/>";
+                    }
+                    else if($opp == "*")
+                    {
+                        echo "The product of the numbers is " . ($int1 * $int2) . "<br/>";
+                    }
+                    else if($opp == "/")
+                    {
+                        if($int2 == 0)
+                        {
+                            throw new Exception("The answer is cannot divide a number by zero <br/>");
+                        }
+                        else
+                        {
+                            echo "The quotient of the numbers is " . ($int1 / $int2) . "<br/>";
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Cannot perform operation. You must have three arguments. A string for the operator 
+(+,-,*,/) and two integers or floats for the numbers. <br/>");
+                    }
+                }
+                catch(exception $e)
+                {
+                    echo $e->getMessage();
+                }
+            }
+    };
+?>
